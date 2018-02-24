07afac9bc35250b5eec89a9f02ff5e43cc735ed2
ShadeWe <slim.shady.ghetto@yandex.ru>
2018-02-24 12:23:37 +0500

    rework the logic

---
+++ ftp-client/colorDefines.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define BLACK			 0
+#define DARKBLUE		 1
+#define GREEN			 2
+#define OCEANIC			 3
+#define RED				 4
+#define DARKPURPLE		 5
+#define GOLD			 6
+#define WHITE			 7
+#define GRAY			 8
+#define BLUE			 9
+#define LIGHTGREEN		10
+#define LIGHTBLUE		11
+#define LIGHTRED		12
+#define PURPLE			13
+#define YELLOW			14
+#define LIGHTWHITE		15
--- ftp-client/main.cpp
+++ ftp-client/main.cpp
@@ -2,33 +2,129 @@
 #include <winsock.h>
 #include <Windows.h>
 #include <string>
+#include "colorDefines.h"
 
 using namespace std;
 
-void processCustomCommands(string command) {
+SOCKET serverSocket;		// contains the socket a user is connecting to.
 
-	if (command == "/gethelp") {
-		cout << "here's the list of commands you can do:" << endl;
-		cout << "HELP PASV PORT LIST PASS USER" << endl;
-	}
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-}
+// these values are set by a user and are needed to connect to a server.
+string hostname;
+string username;
+string password;
+string port;
 
-string cutMessage(char * buffer) {
+char ip_address[15];		// contains the ip address of a domain
+
+bool isConnectionEstablished = false;
+
+string getResponseCode(char * buffer) {
 
 	string response;
 
-	for (int i = 0; true; i++) {
-		if (buffer[i] != '\r') response.push_back(buffer[i]);
-		else break;
+	for (int i = 0; i < 3; i++) {
+		response.push_back(buffer[i]);
 	}
 
 	return response;
 }
 
+void writeLine(string text, int color) {
+
+	SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | color));
+	cout << text << endl;
+	SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 15));
+
+}
+
+void write(string text, int color) {
+
+	SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | color));
+	cout << text;
+	SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 15));
+
+}
+
+int processCommand(string command) {
+
+	if (command == "/gethelp") {
+		cout << "To connect to an FTP server, type in '/connect' and follow instructions" << endl << endl;
+		return 0;
+	}
+	if (command == "/set") {
+
+		write("   hostname >> ", DARKPURPLE);
+		getline(cin, hostname);
+		if (cin.eof()) return 0;
+
+		write("   username >> ", DARKPURPLE);
+		getline(cin, username);
+		if (cin.eof()) return 0;
+
+		write("   password >> ", DARKPURPLE);
+		getline(cin, password);
+		if (cin.eof()) return 0;
+
+		write("   port(21) >> ", DARKPURPLE);
+		getline(cin, port);
+		if (cin.eof()) return 0;
+
+
+		cout << endl;
+		writeLine("Check if you've set everything properly and type '/connect'", LIGHTWHITE);
+		cout << endl;
+
+		return 0;
+	}
+	if (command == "/connect") {
+		cout << "connecting... ";
+
+		hostent * host;   
+
+		host = gethostbyname(hostname.c_str());
+
+		if (host == NULL) {
+			writeLine("something went wrong while getting the IP address...\ncheck if the hostname has been written properly and try again", RED);
+			return 0;
+		}
+		
+		sprintf_s(ip_address, "%s", inet_ntoa(*(in_addr*)host->h_addr_list[0]));
+
+		// the address of the server a user is connecting to
+		SOCKADDR_IN address;
+		address.sin_addr.s_addr = inet_addr(ip_address);
+		address.sin_port = htons(stoi(port));
+		address.sin_family = AF_INET;
+
+		// creating a socket and binding the address to this socket.
+		serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+		bind(serverSocket, (SOCKADDR*)&address, sizeof(address));
+
+		// connecting to the socket
+		connect(serverSocket, (SOCKADDR*)&address, sizeof(address));
+
+		writeLine("done!", LIGHTGREEN);
+
+		char buffer[1024];
+
+		recv(serverSocket, buffer, 1024, 0);
+		string responseCode = getResponseCode(buffer);
+
+		if (responseCode == "220") {
+			isConnectionEstablished = true;
+		}
+		
+		cout << endl;
+
+		return 0;
+	}
+
+}
+
 int main()
 {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 	// here comes winsock startup
 	WSAData wsaData;
@@ -39,111 +135,35 @@ int main()
 		return 0;
 	}
 
-
-	hostent * host;     // contains the ip address of a domain
-	string hostname;    // the hostname typed in by the user
-
-
-						// speedtest.tele2.net / ftp.hq.nasa.gov
-
-	while (true) {
-
-		SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 3));
-		cout << "enter a hostname: ";
-		SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 15));
-
-		cin >> hostname;
-		host = gethostbyname(hostname.c_str());
-
-		if (host == NULL) {
-			SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 4));
-			cout << "something went wrong while getting the IP address...\ncheck if the hostname has been written properly and try again" << endl;
-			SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 7));
-		}
-		else {
-			break;
-		}
-
-	}
-
-	char ip_address[15];
-	sprintf_s(ip_address, "%s", inet_ntoa(*(in_addr*)host->h_addr_list[0]));
-
-
-	cout << endl << "the ip address of the domain specified: ";
-
-	SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 13));
-	cout << ip_address << endl << endl;
-	SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 7));
-
-	// the address of the server a user is connecting to
-	SOCKADDR_IN address;
-	address.sin_addr.s_addr = inet_addr(ip_address);
-	address.sin_port = htons(21);
-	address.sin_family = AF_INET;
-
-	// creating a socket and binding the address to this socket.
-	SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-	bind(serverSocket, (SOCKADDR*)&address, sizeof(address));
-
-	// connecting to the socket
-	connect(serverSocket, (SOCKADDR*)&address, sizeof(address));
+	// speedtest.tele2.net / ftp.hq.nasa.gov
 
 	SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 15));
-	cout << "connection has been established, type in";
+	cout << "Welcome to the FTP-client, type in";
 	SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 3));
 	cout << " '/gethelp' ";
 	SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 15));
 	cout << "to see what you can do" << endl << endl;
 	SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 7));
 
-	char buffer[1024];	// contains everything the server sends back.
-	string command;		// the command sent by a user.
-
-	cin.ignore();
-
-	recv(serverSocket, buffer, 1024, 0);
-	string response = cutMessage(buffer);	// cuttin' the message from all this stuff containing in the buffer
-
-	cout << "the server responded with the code ";
-	cout << response[0] << response[1] << response[2] << endl;
-	cout << "you can send messages now." << endl << endl;
-
+	string command;
 
 	while (true) {
 
-		// getting a command from a user.
-		SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 3));
-		cout << "command to server: ";
-		SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 15));
+		if (isConnectionEstablished == false)
+			write(" command >> ", OCEANIC);
+		else
+			write(" " + hostname + " >> ", OCEANIC);
+
 
 		getline(cin, command);
+		cout << endl;
 
-		if (command[0] == '/') {
-			processCustomCommands(command);
-			continue;
-		}
-		else {
+		processCommand(command);
 
-			command.push_back('\r');
-			command.push_back('\n');
 
-			const char * c = command.c_str();
-
-			send(serverSocket, c, command.length(), 0);
-		}
-
-		recv(serverSocket, buffer, 1024, 0);
-		string response = cutMessage(buffer);
-
-		SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 3));
-		cout << "server's response: ";
-		SetConsoleTextAttribute(hConsole, (WORD)((0 << 4) | 15));
-
-		cout << response << endl << endl;
-
+		cin.clear();
 	}
-
+	
 	system("PAUSE");
 	return 0;
 }
